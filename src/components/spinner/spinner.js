4036a78ce203b86f8a689cd6b39d833886376192
Znogoud <znogoud@pop-os.localdomain>
2021-10-14 18:22:50 +0300

    Add a spinner to the Random Planet component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-dual-ring-ts6k0k1m4pd"><div className="ldio-owfobloensr">
+            <div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
